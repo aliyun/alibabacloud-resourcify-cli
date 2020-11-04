f53013990ea3895c8ca4df9698ed189f2edcbd32
aixiaoming <1049370257@qq.com>
2020-11-04 14:15:34 +0800

    delete useless comments

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import AppContent from './pages';
-// import { DocContent } from './component/content.js';
-// import { SubList } from './component/subList.js';
 
 export interface AppProps {}
 
@@ -16,47 +14,3 @@ const App: React.FC<AppProps> = () => {
 };
 
 export default App;
-
-// class App extends React.Component {
-//   state = {
-//     sublist: {},
-//     activeUrlPath: '',
-//     contentLevel: '',
-//   };
-
-//   componentWillMount() {
-//     Axios.get('/product')
-//       .then((response) => {
-//         this.setState({ sublist: response.data });
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }
-//   render() {
-//     return (
-//       <div className='App'>
-//         <div className='list'>
-//           <SubList
-//             level='product'
-//             sublist={this.state.sublist}
-//             onChange={(data) => {
-//               this.setState({
-//                 activeUrlPath: data.activeUrlPath,
-//                 contentLevel: data.contentLevel,
-//               });
-//             }}
-//           />
-//         </div>
-//         <div className='content'>
-//           <DocContent
-//             activeUrlPath={this.state.activeUrlPath}
-//             contentLevel={this.state.contentLevel}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
