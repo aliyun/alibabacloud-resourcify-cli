b0df98e4fca6f9a8997299f9a0bea7f964bf9e05
liaoyustudent <wb-ly617236@alibaba-inc.com>
2021-01-26 15:10:42 +0800

    supported arc-cs nodepools

--- cmds/meta/arc/cs/nodepool/delete.js
+++ cmds/meta/arc/cs/nodepool/delete.js
@@ -4,27 +4,33 @@ let { default: Client } = require(`@alicloud/cs20151215`);
 let runtime = require('../../../../../lib/runtime.js');
 let output = require('../../../../../lib/output.js');
 
+
 exports.cmdObj = {
   desc: {
-    zh: '查询集群Addons升级状态',
-    en: `query the upgrade status of a cluster add-on.`
+    en: 'Delete the cluster node pool',
+    zh: '删除节点池'
   },
   options: {
-    'component-ids': {
-      required: true,
-      mapping: 'DescribeClusterAddonsUpgradeStatusRequest.componentIds',
-      vtype: 'array',
-      subType: 'string',
-      desc: {
-        zh: '组件名称',
-        en: ''
-      }
-    }
+
   },
   args: [
     {
       name: 'clusterId',
-      required: true
+      required: true,
+      vtype: 'string',
+      desc: {
+        zh: '容器实例Id',
+        en: ''
+      }
+    },
+    {
+      name: 'nodepoolId',
+      required: true,
+      vtype: 'string',
+      desc: {
+        zh: '节点池Id',
+        en: ''
+      }
     }
   ]
 };
@@ -39,14 +45,11 @@ exports.run = async function (ctx) {
     regionId: profile.region,
     type: profile.type
   });
-
-  let DescribeClusterAddonsUpgradeStatusRequest = require(`@alicloud/cs20151215`).DescribeClusterAddonsUpgradeStatusRequest;
-  let request = new DescribeClusterAddonsUpgradeStatusRequest(ctx.mappingValue.DescribeClusterAddonsUpgradeStatusRequest);
-
+  
   let client = new Client(config);
   let result;
   try {
-    result = await client.describeClusterAddonsUpgradeStatusWithOptions(ctx.argv[0], request, {}, runtime.getRuntimeOption());
+    result = await client.deleteClusterNodepoolWithOptions(ctx.argv[0], ctx.argv[1], {}, runtime.getRuntimeOption());
   } catch (e) {
     output.error(e.message);
   }
@@ -55,4 +58,5 @@ exports.run = async function (ctx) {
   }
   let data = JSON.stringify(result, null, 2);
   output.log(data);
+
 };
