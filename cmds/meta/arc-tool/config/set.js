b4cb290c2f3a763998062049e4e9e7a3d4778d4e
Jackson Tian <shyvo1987@gmail.com>
2021-07-27 15:38:35 +0800

    fix config

--- cmds/meta/arc-tool/config/set.js
+++ cmds/meta/arc-tool/config/set.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const config = require('../../../../lib/config.js');
+const Config = require('../../../../lib/config.js');
 
 exports.cmdObj = {
   desc: {
@@ -32,5 +32,6 @@ exports.run = function (ctx) {
   } else {
     ctx.profile[ctx.argv[0]] = ctx.argv[1];
   }
+  const config = new Config();
   config.updateProfile(ctx.profileName, ctx.profile);
 };
