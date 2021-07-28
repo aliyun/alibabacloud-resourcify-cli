7f2abddcc529531ce95c3b63671029dfab66e9ab
Jackson Tian <shyvo1987@gmail.com>
2021-07-28 11:24:18 +0800

    add test cases with child process

--- bin/arc.js
+++ bin/arc.js
@@ -9,4 +9,4 @@ const path = require('path');
 const { run } = require('../lib/run.js');
 
 // 运行
-run('arc', path.join(__dirname, '../cmds/meta/arc'));
+run('arc', path.join(__dirname, '../cmds/meta/arc'), process.argv.slice(2));
---
+++ test/arc.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const cp = require('child_process');
+
+function spawn(modulePath, args, options) {
+  return new Promise((resolve, reject) => {
+    const p = cp.spawn(modulePath, args, options);
+    const stdout = [];
+    p.stdout.on('data', (chunk) => {
+      stdout.push(chunk);
+    });
+
+    const stderr = [];
+    p.stderr.on('data', (chunk) => {
+      stderr.push(chunk);
+    });
+
+    p.on('exit', (code) => {
+      resolve({
+        stdout: Buffer.concat(stdout).toString('utf-8'),
+        stderr: Buffer.concat(stderr).toString('utf-8'),
+        code: code
+      });
+    });
+
+    p.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+describe('arc', function () {
+  it('arc', async () => {
+    const {code, stdout, stderr} = await spawn(path.join(__dirname, '../bin/arc.js'));
+    assert.strictEqual(code, 1);
+    assert.strictEqual(stdout, `usage:
+    arc  [子命令]
+    arc  [选项]
+
+  阿里云资源化命令行工具，用于云服务资源操作
+
+子命令:
+    cs                                  容器服务
+选项:
+  --profile                             [string]            指定要使用的配置文件
+  --region                              [string]            指定阿里云区域
+  -i,--interaction                      [boolean]           交互式填充参数
+`);
+    assert.strictEqual(stderr, '');
+  });
+
+  it('arc-tool', async () => {
+    const {code, stdout, stderr} = await spawn(path.join(__dirname, '../bin/arc-tool.js'));
+    assert.strictEqual(code, 1);
+    assert.strictEqual(stdout, `usage:
+    arc-tool  [子命令]
+
+  阿里云资源化命令行工具，用于配置、自动补全等辅助设置
+
+子命令:
+    config                              配置CLI
+    completion                          自动补全
+    serve                               启动帮助文档web服务器
+`);
+    assert.strictEqual(stderr, '');
+  });
+});
