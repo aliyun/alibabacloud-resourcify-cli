579a4f609dc7ae28a71560f60823d46c4bfa622c
Jackson Tian <shyvo1987@gmail.com>
2021-07-29 22:29:15 +0800

    refine config list/config get command

--- cmds/meta/arc-tool/config/list.js
+++ cmds/meta/arc-tool/config/list.js
@@ -1,21 +1,27 @@
 'use strict';
+
 const Config = require('../../../../lib/config.js');
+const Command = require('../../../../lib/command');
 
-exports.cmdObj = {
-  usage: ['arc-tool config list [--profile profileName]'],
-  desc: {
-    zh: '列举指定配置所有信息，未指定则返回默认',
-    en: `List all the information of the specified configuration, return to the default if not specified`
-  }
-};
-
-exports.run = function () {
-  const config = new Config();
-  const conf = config.getConfig();
-  if (!conf) {
-    console.error('No configuration currently exists');
-    return;
+module.exports = class extends Command {
+  constructor(name) {
+    super(name, {
+      usage: ['arc-tool config list'],
+      desc: {
+        zh: '列举指定配置所有信息，未指定则返回默认',
+        en: `List all the information of the specified configuration, return to the default if not specified`
+      }
+    });
   }
 
-  console.log(JSON.stringify(conf, null, 2));
+  async run(args) {
+    const config = new Config();
+    const conf = config.getConfig();
+    if (!conf) {
+      console.error('No configuration currently exists');
+      return;
+    }
+
+    console.log(JSON.stringify(conf, null, 2));
+  }
 };
