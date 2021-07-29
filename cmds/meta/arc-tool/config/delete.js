6204ec47d5e648a46f493fb6fe85cc0cc697945d
Jackson Tian <shyvo1987@gmail.com>
2021-07-29 23:35:07 +0800

    improve config commands

--- cmds/meta/arc-tool/config/delete.js
+++ cmds/meta/arc-tool/config/delete.js
@@ -1,24 +1,35 @@
 'use strict';
 
+const Command = require('../../../../lib/command.js');
 const Config = require('../../../../lib/config.js');
+const { loadContext } = require('../../../../lib/context.js');
 
-exports.cmdObj = {
-  usage: [
-    'arc config delete <--profile profileName>'
-  ],
-  desc: {
-    zh: '删除配置',
-    en: `remove profile`
-  },
-  args: [
-    {
-      name: 'profileName',
-      required: true
+module.exports = class extends Command {
+  constructor(name) {
+    super(name, {
+      desc: {
+        zh: '删除配置',
+        en: `remove profile`
+      },
+      args: [
+        {
+          name: 'profileName',
+          required: true
+        }
+      ]
+    });
+  }
+
+  async run(args) {
+    const ctx = loadContext(args);
+    this.validateOptions(ctx.parsed);
+    const [ profileName ] = ctx.argv;
+    if (!profileName) {
+      console.error(`the profileName is required`);
+      process.exit(1);
     }
-  ]
-};
 
-exports.run = function (ctx) {
-  const config = new Config();
-  config.delete(ctx.argv[0]);
+    const config = new Config();
+    config.delete(ctx.argv[0]);
+  }
 };
--- cmds/meta/arc-tool/config/set.js
+++ cmds/meta/arc-tool/config/set.js
@@ -1,37 +1,44 @@
 'use strict';
 
+const Command = require('../../../../lib/command.js');
 const Config = require('../../../../lib/config.js');
+const { loadContext } = require('../../../../lib/context.js');
 
-exports.cmdObj = {
-  desc: {
-    zh: '设置配置中的字段，当字段不存在时添加；当value为空时，代表删除指定字段；若只是指定profile，则表示更改默认配置',
-    en: `Set the field in the configuration, add it when the field does not exist; when the value is empty, it means delete the specified field; if only specify the profile, it means change the default configuration`
-  },
-  usage: [
-    'arc config set [<key> <value>] [--profile profileName]'
-  ],
-  args: [
-    {
-      name: 'key',
-    },
-    {
-      name: 'value',
+module.exports = class extends Command {
+  constructor(name) {
+    super(name, {
+      desc: {
+        zh: '设置配置中的字段，当字段不存在时添加；当value为空时，代表删除指定字段；若只是指定profile，则表示更改默认配置',
+        en: `Set the field in the configuration, add it when the field does not exist; when the value is empty, it means delete the specified field; if only specify the profile, it means change the default configuration`
+      },
+      usage: [
+        'arc config set [<key> <value>] [--profile profileName]'
+      ],
+      args: [
+        {
+          name: 'key',
+        },
+        {
+          name: 'value',
+        }
+      ]
+    });
+  }
+  validate (args) {
+    // TODO
+    if (args.argv[0] && args.argv[1] === undefined) {
+      return 'value master be set';
     }
-  ]
-};
-
-exports.validate = function (args) {
-  if (args.argv[0] && args.argv[1] === undefined) {
-    return 'value master be set';
   }
-};
 
-exports.run = function (ctx) {
-  if (!ctx.argv[1]) {
-    delete ctx.profile[ctx.argv[0]];
-  } else {
-    ctx.profile[ctx.argv[0]] = ctx.argv[1];
+  async run(args) {
+    const ctx = loadContext(args);
+    if (!ctx.argv[1]) {
+      delete ctx.profile[ctx.argv[0]];
+    } else {
+      ctx.profile[ctx.argv[0]] = ctx.argv[1];
+    }
+    const config = new Config();
+    config.updateProfile(ctx.profileName, ctx.profile);
   }
-  const config = new Config();
-  config.updateProfile(ctx.profileName, ctx.profile);
 };
