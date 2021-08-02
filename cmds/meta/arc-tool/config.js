fdd991fa7c46d5a12a196b89ef07d351ca5b8745
Jackson Tian <shyvo1987@gmail.com>
2021-08-02 12:55:21 +0800

    fix test cases

--- cmds/meta/arc-tool/config.js
+++ cmds/meta/arc-tool/config.js
@@ -55,24 +55,6 @@ module.exports = class extends Command {
         zh: '交互式配置CLI，根据提示输入参数值，完成后自动将现有配置作为默认配置',
         en: `Configure the CLI interactively, enter parameter values according to the prompts, and automatically use the existing configuration as the default configuration after completion`
       },
-      sub: {
-        delete: {
-          zh: '删除配置',
-          en: `remove profile`
-        },
-        get: {
-          zh: '获取配置指定字段值',
-          en: `Get profile specified field value`
-        },
-        list: {
-          zh: '获取指定配置所有信息',
-          en: `Get all information of the specified configuration`
-        },
-        set: {
-          zh: '设置配置字段值',
-          en: `Set profile field value`
-        }
-      },
       options: {
         'access-key-id': {
           required: true,
