cbd57a0160a7f792af16b5ea0ebdc0223f0f2d76
Jackson Tian <shyvo1987@gmail.com>
2023-01-11 14:52:19 +0800

    fix mapping value

--- test/context.test.js
+++ test/context.test.js
@@ -48,4 +48,42 @@ describe('context.js', function () {
       'profileName': 'default'
     });
   });
+
+  it('load context with definition should ok', function () {
+    const ctx = loadContext(['--region', 'cn-hangzhou'], {
+      'region': {
+        mapping: 'DescribeAddonsRequest.region',
+        vtype: 'string',
+        desc: {
+          zh: '阿里云区域',
+          en: `The ID of the region to query.`
+        }
+      },
+      'cluster-type': {
+        mapping: 'DescribeAddonsRequest.clusterType',
+        desc: {
+          zh: '集群类型，默认为kubernetes',
+          en: `The type of the cluster. Default value: kubernetes.`
+        }
+      }
+    });
+    assert.deepStrictEqual(ctx, {
+      'argv': [],
+      'mappingValue': {
+        'DescribeAddonsRequest': {
+          'clusterType': undefined,
+          'region': 'cn-hangzhou'
+        }
+      },
+      'language': 'zh',
+      'parsed': new Map([['region', 'cn-hangzhou']]),
+      'profile': {
+        'access_key_id': 'id',
+        'access_key_secret': 'secret',
+        'language': 'zh',
+        'region': 'cn-hangzhou'
+      },
+      'profileName': 'default'
+    });
+  });
 });
